46d9af016d5004b516c75b15a32b422ef64da0e8
Radek <chudyksg@gmail.com>
2019-11-05 07:36:17 +0000

    Exercise 1 - Phonebook backend - step 2 & 3

--- Phonebook/index.js
+++ Phonebook/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
+app.use(bodyParser.json())
 
 let persons = [
     {
@@ -31,6 +33,35 @@ app.get('/', (req, res) => {
 
 app.get('/persons', (req, res) => {
     res.json(persons)
+})
+
+app.get('/persons/info', (request, response) => {
+
+})
+
+app.post('/persons', (request, response) => {
+    const person = request.body
+    console.log(person)
+  
+    response.json(person)
+  })
+
+app.get('/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
 })
 
 const PORT = 3001
